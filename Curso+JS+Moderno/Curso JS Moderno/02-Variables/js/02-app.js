d6fc0e7709428e8c7d4f4bcbd9a2464bb3dc09cd
Dixon G <dxxn0101@gmail.com>
2022-11-20 12:47:02 -0600

    Todo lo que debes saber de las variables con let

---
+++ Curso+JS+Moderno/Curso JS Moderno/02-Variables/js/02-app.js
@@ -0,0 +1,15 @@
+let producto = "Tablet";
+
+// let permite reasignar el valor
+
+producto = "Memoria RAM";
+
+console.log(producto);
+
+let precio;
+
+console.log(precio); // undefined
+
+precio = `$50`;
+
+console.log(precio);

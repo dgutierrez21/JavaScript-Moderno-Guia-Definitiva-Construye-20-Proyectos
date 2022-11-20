4904749d425e7e23b193e3243e8f29589c1a90d5
Dixon G <dxxn0101@gmail.com>
2022-11-20 17:30:40 -0600

    String Methods - Convertir a Mayusculas o Minusculas

---
+++ Curso+JS+Moderno/Curso JS Moderno/03-Strings/js/07-app.js
@@ -0,0 +1,17 @@
+const producto = "MonItOr de 24 PulGadas";
+
+// el metodo toUpperCase() lo transforma todo a mayusculas
+console.log(producto.toUpperCase());
+
+// el metodo toLowerCase() lo transforma todo a minusculas
+
+console.log(producto.toLowerCase());
+
+const correo = "CORREO@CORREO.COM";
+
+console.log(correo.toLowerCase());
+
+const precio = 100;
+
+console.log(precio);
+console.log(precio.toString());

ab4dc99ae628b30429bbc147aff19e0fd8b6096c
Dixon G <dxxn0101@gmail.com>
2022-11-20 09:32:37 -0600

    introduccion 04

--- Curso+JS+Moderno/Curso JS Moderno/01-Introduccion/js/main.js
+++ Curso+JS+Moderno/Curso JS Moderno/01-Introduccion/js/main.js
@@ -1,4 +1,16 @@
+// Pregunta al usuario su nombre
 const nombre = prompt("Cual es tu nombre?");
+
+// Toma el nombre y lo muestra en pantalla
 document.querySelector(
   ".contenido"
 ).innerHTML = `${nombre} está aprendiendo JavaScript`;
+
+// Este es un comentario de una linea
+
+/* Este es un
+comentario
+de varios 
+lineas */
+
+const producto = "Teclado 60%";

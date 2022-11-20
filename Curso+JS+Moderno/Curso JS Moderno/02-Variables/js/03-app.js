abd50a49a5b3ae1231a473b68559bb1ba99ac059
Dixon G <dxxn0101@gmail.com>
2022-11-20 12:51:06 -0600

    Todo lo que debes saber de las variables con const

---
+++ Curso+JS+Moderno/Curso JS Moderno/02-Variables/js/03-app.js
@@ -0,0 +1,10 @@
+// Const no puede ser reasignado
+const producto = "Celular";
+
+producto = "Tablet";
+
+// console.log(producto) // Uncaught TypeError: Assignment to constant variable.
+
+// Las constantes se deben inicializar
+
+// const producto2; // Error

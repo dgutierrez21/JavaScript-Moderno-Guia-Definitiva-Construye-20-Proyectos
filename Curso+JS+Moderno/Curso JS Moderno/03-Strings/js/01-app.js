2909e5ed7da1db0b660bef233b1f3070c7df880c
Dixon G <dxxn0101@gmail.com>
2022-11-20 14:25:36 -0600

    Crear Strings en JavaScript

---
+++ Curso+JS+Moderno/Curso JS Moderno/03-Strings/js/01-app.js
@@ -0,0 +1,13 @@
+// Los string o cadena de texto, representan un texto.
+// Se puede usar por ejemplo para almacenar el nombre de un cliente.
+
+// Existen tres formas de crear strings
+const producto = "Monitor de 20'";
+
+const producto2 = String('Monitor 24 "pulgadas"');
+
+const producto3 = new String("Monitor 32 pulgadas");
+
+console.log(producto);
+console.log(producto2);
+console.log(producto3);

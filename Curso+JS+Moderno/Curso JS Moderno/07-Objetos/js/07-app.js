47e94422e00bf4aa4f373989d52825cfbc2b14a7
Dixon G <dxxn0101@gmail.com>
2022-11-21 16:40:09 -0600

    El Problema con los objetos

---
+++ Curso+JS+Moderno/Curso JS Moderno/07-Objetos/js/07-app.js
@@ -0,0 +1,11 @@
+const producto = {
+  nombre: "Mouse Logitech M720 Triathlon",
+  precio: 34.99,
+  disponible: true,
+};
+
+// const nombreProducto = "Mouse";
+
+// nombreProducto = "Teclado"; // Uncaught TypeError: Assignment to constant variable.
+
+//a diferencia de una variable, Las propiedades de los objetos si se pueden reasignar

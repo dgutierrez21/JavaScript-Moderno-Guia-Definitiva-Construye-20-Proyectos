ae6c8dbb1897805cfce22bf6ed38f9ce65410a91
Dixon G <dxxn0101@gmail.com>
2022-11-21 15:21:18 -0600

    Como Acceder a los valores de un objeto

---
+++ Curso+JS+Moderno/Curso JS Moderno/07-Objetos/js/02-app.js
@@ -0,0 +1,16 @@
+const producto = {
+  nombre: "Mouse Logitech M720 Triathlon",
+  precio: 34.99,
+  disponible: true,
+};
+
+console.log(producto);
+
+// Acceder a las propiedades del objeto usando notacion de punto
+console.log(producto.nombre);
+console.log(producto.precio);
+console.log(producto.disponible);
+
+// Acceder a las propiedades del objeto usando notacion de corchetes
+
+console.log(producto["nombre"]);

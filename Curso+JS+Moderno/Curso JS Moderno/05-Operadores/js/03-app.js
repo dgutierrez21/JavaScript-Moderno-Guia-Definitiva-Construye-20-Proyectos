87085b4b7dcf301955201e447e50de55c85901ec
Dixon G <dxxn0101@gmail.com>
2022-11-21 13:07:11 -0600

    Comprar Null y undefined en JavaScript

---
+++ Curso+JS+Moderno/Curso JS Moderno/05-Operadores/js/03-app.js
@@ -0,0 +1,18 @@
+// undefined
+let numero;
+
+console.log(numero); // undefined
+
+// null
+// "null' significa "dato desconocido" o "valor inexistente".
+
+let num2 = null;
+
+console.log(num2); // null
+
+console.log(typeof num2);
+
+console.log(numero == num2); // true
+
+// Nota: siempre usar el comparador estricto
+console.log(numero === num2); // false

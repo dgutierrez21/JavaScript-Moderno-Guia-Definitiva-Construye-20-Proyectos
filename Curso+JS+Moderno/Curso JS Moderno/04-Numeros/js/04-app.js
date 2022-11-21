338b050f2ab13afff50a3154b52db6d3359dc98e
Dixon G <dxxn0101@gmail.com>
2022-11-21 10:12:39 -0600

    El Orden de las operaciones

---
+++ Curso+JS+Moderno/Curso JS Moderno/04-Numeros/js/04-app.js
@@ -0,0 +1,19 @@
+let resultado;
+
+// Orden de operaciones === primero se hace la division, luego la multiplicacion, y de ultimo la suma y resta
+
+resultado = 30 + 50 * 2; // 50 * 2 + 30 = 130
+
+// Si se desea hacer primero la suma se usa un parentesis
+
+resultado = (20 + 50) * 2; // = 140
+
+// 20% de descuento en el carrito de compras
+
+resultado = (20 + 30 + 40 + 50 + 60) * 0.2; // 40
+
+// Impuestos
+
+resultado = (20 + 30) * 1.16; // 57.999...
+
+console.log(resultado);

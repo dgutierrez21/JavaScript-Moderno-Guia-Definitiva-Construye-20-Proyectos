cb46a2a63747a324dc249793f37c83ede86a0f08
Dixon G <dxxn0101@gmail.com>
2022-11-21 15:17:04 -0600

    Crear Objetos en JavaScript

---
+++ Curso+JS+Moderno/Curso JS Moderno/07-Objetos/js/01-app.js
@@ -0,0 +1,14 @@
+const nombre = "Mouse Logitech M720 Triathlon",
+  precio = 34.99,
+  disponible = true;
+
+// En lugar de crear muchas variables, se puede usar un objeto
+
+// Objecto literal
+const producto = {
+  nombre: "Mouse Logitech M720 Triathlon",
+  precio: 34.99,
+  disponible: true,
+};
+
+console.log(producto);

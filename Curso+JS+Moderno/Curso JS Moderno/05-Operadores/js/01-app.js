4628ee63398ee17910e67b8efb7348271ec9a136
Dixon G <dxxn0101@gmail.com>
2022-11-21 12:36:39 -0600

    Operador Mayor que y Menor Que

---
+++ Curso+JS+Moderno/Curso JS Moderno/05-Operadores/js/01-app.js
@@ -0,0 +1,17 @@
+const num1 = 20,
+  num2 = "20",
+  num3 = 30;
+
+// Operador mayor a
+
+console.log(num3 > num1); // true
+console.log(num1 > num3); // false
+
+console.log(num3 > num2); // true
+
+// Operador menor a
+
+console.log(num3 < num1); // false
+console.log(num1 < num3); // true
+
+console.log(num3 < num2); // false

9b8ba6d7b64f2616c8ac655e1b19197e257c9128
Dixon G <dxxn0101@gmail.com>
2022-11-20 18:46:43 -0600

    Crear Números en JavaScript

---
+++ Curso+JS+Moderno/Curso JS Moderno/04-Numeros/js/01-app.js
@@ -0,0 +1,17 @@
+const num1 = 54,
+  num2 = 25,
+  num3 = 93.44,
+  num4 = 0.32423,
+  num5 = -20,
+  num6 = "54";
+
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+console.log(num5);
+console.log(10 + num6);
+
+const num7 = new Number(34);
+
+console.log(num7);

e874a4354607a903861e52294b92b77ff362310e
Dixon G <dxxn0101@gmail.com>
2022-11-20 19:25:10 -0600

    Operaciones en JavaScript

---
+++ Curso+JS+Moderno/Curso JS Moderno/04-Numeros/js/02-app.js
@@ -0,0 +1,26 @@
+const num1 = 50,
+  num2 = 30;
+
+let resultado;
+
+// Suma
+
+resultado = num1 + num2;
+
+// Resta
+
+resultado = num1 - num2;
+
+// multiplicacion
+
+resultado = num1 * num2;
+
+// division
+
+resultado = num1 / num2;
+
+// modulo
+
+resultado = num1 % num2; // residuo de la division
+
+console.log(resultado);

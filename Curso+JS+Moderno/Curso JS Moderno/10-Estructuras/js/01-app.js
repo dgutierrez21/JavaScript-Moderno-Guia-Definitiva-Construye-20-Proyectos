15a3a94d89e1b25c413c2f9d754a7e8305ec3dbc
Dixon G <dxxn0101@gmail.com>
2022-11-23 13:27:20 -0600

    Creando un If

---
+++ Curso+JS+Moderno/Curso JS Moderno/10-Estructuras/js/01-app.js
@@ -0,0 +1,13 @@
+// if else
+
+const puntaje = 1000;
+
+if (puntaje == 1000) {
+  console.log(
+    "Si la condición es verdadera, ejecuta este código TRUE | TRUE | TRUE | TRUE"
+  );
+} else {
+  console.log(
+    "Si la condición no es falsa, ejecuta este otro código FALSE | FALSE | FALSE"
+  );
+}

242a9b760c0fdaa34c5ca500cd3c34ede84e6189
Dixon G <dxxn0101@gmail.com>
2022-11-22 20:12:01 -0600

    La Diferencia entre Función y Método

---
+++ Curso+JS+Moderno/Curso JS Moderno/09-Funciones/js/04-app.js
@@ -0,0 +1,10 @@
+// La Diferencia entre Función y Método
+
+const num1 = 10,
+  num2 = "30";
+
+console.log(parseInt(num3)); // esta es una funcion
+
+console.log(num1.toString()); // este es un metodo
+
+//  Llamas a una función una función cuando está fuera de un objeto o clase, y llamas método a una función cuando está escrita dentro de una clase o objeto. ... Si está diciendo métodos, quiere decir funciones dentro de la clase o objeto. Si está diciendo funciones, quiere decir simplemente funciones fuera de la clase o objeto.

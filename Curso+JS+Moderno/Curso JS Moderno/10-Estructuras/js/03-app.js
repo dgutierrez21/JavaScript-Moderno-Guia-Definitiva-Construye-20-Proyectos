ec0cdc02c583b13a9aa4ad9a07a4e62553a308f6
Dixon G <dxxn0101@gmail.com>
2022-11-23 14:03:24 -0600

    Comparando Mayor o Menor que

---
+++ Curso+JS+Moderno/Curso JS Moderno/10-Estructuras/js/03-app.js
@@ -0,0 +1,9 @@
+const presupuesto = 500,
+  totalPagar = 500;
+
+if (presupuesto >= totalPagar) {
+  // presupuesto es mayor o igual a totalPagar ???
+  console.log("Su compra se ha realizado correctamente");
+} else {
+  console.log("Fondos insuficientes");
+}

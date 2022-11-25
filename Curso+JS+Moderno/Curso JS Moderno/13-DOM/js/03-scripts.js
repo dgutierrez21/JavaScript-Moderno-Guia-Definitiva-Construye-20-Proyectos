79f96491cd831af2c8d6a0e8438019b9243f5ec2
Dixon G <dxxn0101@gmail.com>
2022-11-25 14:53:08 -0600

    getElementById

---
+++ Curso+JS+Moderno/Curso JS Moderno/13-DOM/js/03-scripts.js
@@ -0,0 +1,8 @@
+const form = document.getElementById("formulario");
+//NOTA: Si hay dos elementos con el mismo id, solo va a devolver el primero que encuentre.
+console.log(form);
+
+const noExiste = document.getElementById("no-existe");
+// Si el elemento no existe, devuelve null
+
+console.log(noExiste);

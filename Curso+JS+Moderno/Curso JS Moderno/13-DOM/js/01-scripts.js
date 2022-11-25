4613c79c08a999655f88b4d5e7bde158d36b1c39
Dixon G <dxxn0101@gmail.com>
2022-11-25 14:23:31 -0600

    Acceder a elementos del HTML con document

---
+++ Curso+JS+Moderno/Curso JS Moderno/13-DOM/js/01-scripts.js
@@ -0,0 +1,25 @@
+let elemento;
+
+elemento = document;
+elemento = document.body;
+elemento = document.all;
+elemento = document.title;
+elemento = document.domain;
+elemento = document.forms;
+elemento = document.forms[0];
+elemento = document.forms[0].id;
+elemento = document.forms[0].method;
+elemento = document.forms[0].classList;
+elemento = document.forms[0].className;
+elemento = document.forms[0].action;
+
+elemento = document.links;
+elemento = document.links[4];
+elemento = document.links[4].classList;
+elemento = document.links[4].className;
+
+elemento = document.images;
+
+elemento = document.scripts;
+
+console.log(elemento);

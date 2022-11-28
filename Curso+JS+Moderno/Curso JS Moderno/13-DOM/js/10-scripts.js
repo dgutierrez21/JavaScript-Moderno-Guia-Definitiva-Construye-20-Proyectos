ab7403523157612c54af2432b67c097f2325def9
Dixon G <dxxn0101@gmail.com>
2022-11-28 12:15:21 -0600

    Generar html con JavaScript 2

--- Curso+JS+Moderno/Curso JS Moderno/13-DOM/js/10-scripts.js
+++ Curso+JS+Moderno/Curso JS Moderno/13-DOM/js/10-scripts.js
@@ -39,3 +39,61 @@ const nav = document.querySelector(".navegacion");
 const hijoAgregado = nav.insertBefore(enlace, nav.children[1]);
 
 console.log(hijoAgregado);
+
+// EJEMPLO CREANDO NUEVO CARDS
+const info = document.createElement("div"),
+  parr1 = document.createElement("p"),
+  parr2 = document.createElement("p"),
+  parr3 = document.createElement("p");
+
+parr1.textContent = "Concierto";
+parr1.classList.add("categoria", "concierto");
+
+console.log(parr1);
+
+parr2.textContent = "Concierto de Rock JS";
+parr2.classList.add("titulo");
+
+console.log(parr2);
+
+parr3.textContent = "$800 por persona";
+parr3.classList.add("precio");
+
+console.log(parr3);
+
+// div info
+
+info.classList.add("info");
+
+console.log(info);
+
+const parrafos = [parr1, parr2, parr3];
+
+for (const parrafo of parrafos) {
+  info.appendChild(parrafo);
+}
+// Crear imagen
+const img = document.createElement("img");
+img.src = "img/hacer2.jpg";
+
+// Crear div card
+const card = document.createElement("div");
+card.classList.add("card");
+
+// Asignar elementos hijos del card
+
+const hijosCard = [img, info];
+
+for (const hijo of hijosCard) {
+  card.appendChild(hijo);
+}
+
+console.log(card);
+
+// Insertar en el html
+
+const contenedorCards = document.querySelector(".hacer .contenedor-cards");
+contenedorCards.insertBefore(
+  card,
+  contenedorCards.children[contenedorCards.children.length - 1]
+);

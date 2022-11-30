2b82915fbdab1787f0c5d743c2827b3d389ec120
Dixon G <dxxn0101@gmail.com>
2022-11-29 20:36:51 -0600

    Eventos ideales para Validar un formulario

--- Curso+JS+Moderno/Curso JS Moderno/16-PROYECTO-EnviarEmail/js/app.js
+++ Curso+JS+Moderno/Curso JS Moderno/16-PROYECTO-EnviarEmail/js/app.js
@@ -1,5 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // Variables
   const inputEmail = document.querySelector("#email"),
     inputAsunto = document.querySelector("#asunto"),
     inputMensaje = document.querySelector("#mensaje");
+
+  // Eventos
+  inputEmail.addEventListener("blur", (e) => {
+    console.log(e.target.value);
+  });
 });

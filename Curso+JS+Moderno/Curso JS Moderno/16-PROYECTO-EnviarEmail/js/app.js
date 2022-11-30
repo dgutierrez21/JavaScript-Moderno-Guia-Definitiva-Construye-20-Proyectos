8a3ce730a05fbb6f2cc1071b5f52b5a2f2b8dec1
Dixon G <dxxn0101@gmail.com>
2022-11-29 20:07:58 -0600

    Primeros Pasos con el Proyecto EnviarEmail

---
+++ Curso+JS+Moderno/Curso JS Moderno/16-PROYECTO-EnviarEmail/js/app.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const inputEmail = document.querySelector("#email"),
+    inputAsunto = document.querySelector("#asunto"),
+    inputMensaje = document.querySelector("#mensaje");
+});

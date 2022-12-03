965898cc0a0d0fb7b70b09a5eaf2b0a33f6689bf
Dixon G <dxxn0101@gmail.com>
2022-12-03 15:15:52 -0600

    Validando el Formulario

--- Curso+JS+Moderno/Curso JS Moderno/19-PROYECTO-LocalStorage/js/app.js
+++ Curso+JS+Moderno/Curso JS Moderno/19-PROYECTO-LocalStorage/js/app.js
@@ -17,5 +17,31 @@ function eventos() {
 function agregarTweet(evento) {
   evento.preventDefault();
 
+  const tweet = document.querySelector("#tweet").value;
+
+  if (!tweet) {
+    mostrarError("El tweet no puede ir vacio");
+
+    return;
+  }
+
   console.log("Agregando tweet");
+}
+
+// mostrar mensaje de error
+function mostrarError(error) {
+  if (document.querySelector(".error")) {
+    return;
+  }
+
+  const msjError = document.createElement("p");
+  msjError.classList.add("error");
+  msjError.textContent = error;
+
+  const contenido = document.querySelector("#contenido");
+  contenido.appendChild(msjError);
+
+  setTimeout(() => {
+    msjError.remove();
+  }, 3000);
 }

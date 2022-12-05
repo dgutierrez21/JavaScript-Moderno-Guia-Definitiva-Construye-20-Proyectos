04ee6d92c4d7f4893f7b4ede7ffc14990c393b88
Dixon G <dxxn0101@gmail.com>
2022-12-05 16:32:48 -0600

    Métodos y Métodos estaticos en las classes

--- Curso+JS+Moderno/Curso JS Moderno/24-POO/js/01-app.js
+++ Curso+JS+Moderno/Curso JS Moderno/24-POO/js/01-app.js
@@ -4,11 +4,25 @@ class Cliente {
     this.nombre = nombre;
     this.saldo = saldo;
   }
+
+  mostrarInformacion() {
+    return `El nombre del cliente es ${this.nombre} y su saldo es de ${this.saldo}`;
+  }
+
+  // Los métodos estaticos son propios de la clase, no se puede acceder mediante una instancia
+  //los métodos estaticos no requieren una instancia
+  // Los métodos estaticos no pueden acceder a las propiedades de la clase
+
+  static bienvenida() {
+    return `Hola, Bienvenido/a al cajero`;
+  }
 }
 
 const pablo = new Cliente("Pablo", 600);
 
 console.log(pablo);
+console.log(pablo.mostrarInformacion());
+console.log(Cliente.bienvenida());
 
 // expresion de clase
 const Cliente2 = class {
@@ -16,8 +30,21 @@ const Cliente2 = class {
     this.nombre = nombre;
     this.saldo = saldo;
   }
+
+  mostrarInformacion() {
+    return `El nombre del cliente es ${this.nombre} y su saldo es de ${this.saldo}`;
+  }
+
+  // Los métodos estaticos son propios de la clase, no se puede acceder mediante una instancia
+  //los métodos estaticos no requieren una instancia
+  // Los métodos estaticos no pueden acceder a las propiedades de la clase
+  static bienvenida() {
+    return `Hola, Bienvenido/a al cajero`;
+  }
 };
 
 const juan = new Cliente2("Juan", 300);
 
 console.log(juan);
+console.log(juan.mostrarInformacion());
+console.log(Cliente2.bienvenida());

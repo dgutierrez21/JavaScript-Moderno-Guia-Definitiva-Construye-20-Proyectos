30e22d3afa539f293309d47a97bf177ead92f359
Dixon G <dxxn0101@gmail.com>
2022-12-06 12:00:03 -0600

    Validación del Formulario

--- Curso+JS+Moderno/Curso JS Moderno/25-PROYECTO-GastoSemanal/js/app.js
+++ Curso+JS+Moderno/Curso JS Moderno/25-PROYECTO-GastoSemanal/js/app.js
@@ -9,6 +9,8 @@ eventos();
 
 function eventos() {
   document.addEventListener("DOMContentLoaded", preguntarPresupuesto);
+
+  form.addEventListener("submit", agregarGasto);
 }
 
 // clases
@@ -29,6 +31,35 @@ class UI {
     document.querySelector("#total").textContent = presupuesto;
     document.querySelector("#restante").textContent = presupuestoRestante;
   }
+
+  imprimirAlerta(mensaje, tipo) {
+    // validar si ya existe el divMensaje
+    if (document.querySelector(".divMensaje")) {
+      return;
+    }
+    // crear div
+    const divMensaje = document.createElement("div");
+    divMensaje.classList.add("text-center", "alert", "divMensaje");
+
+    if (tipo === "error") {
+      divMensaje.classList.add("alert-danger");
+    } else {
+      divMensaje.classList.add("alert-success");
+    }
+
+    // mensaje error
+    divMensaje.textContent = mensaje;
+
+    // insertar en el html
+
+    document.querySelector(".primario").insertBefore(divMensaje, form);
+
+    setTimeout(() => {
+      divMensaje.remove();
+    }, 3000);
+  }
+
+  // quitar el html
 }
 
 // Intancias
@@ -48,4 +79,27 @@ function preguntarPresupuesto() {
   presupuesto = new Presupuesto(presupuestoUsuario);
 
   ui.insertarPresupuesto(presupuesto);
+}
+
+// añadir gastos
+function agregarGasto(e) {
+  e.preventDefault();
+
+  // leer datos del formulario
+  const nombre = document.querySelector("#gasto").value,
+    cantidad = Number(document.querySelector("#cantidad").value);
+
+  if (!nombre && !cantidad) {
+    ui.imprimirAlerta("Ambos campos son obligatorios", "error");
+
+    return;
+  }
+
+  if (!cantidad || cantidad < 1) {
+    ui.imprimirAlerta("Cantidad no válida.", "error");
+
+    return;
+  }
+
+  ui.imprimirAlerta("Agregando gastos...", "exito");
 }

3f7b50257a252ec2de8e7c3d6bea654fdd418c3e
Dixon G <dxxn0101@gmail.com>
2022-12-07 21:41:47 -0600

    Validación y Mostrar un mensaje de error

--- Curso+JS+Moderno/Curso JS Moderno/26-PROYECTO-AdministrarCitas/js/app.js
+++ Curso+JS+Moderno/Curso JS Moderno/26-PROYECTO-AdministrarCitas/js/app.js
@@ -11,6 +11,56 @@ const form = document.querySelector("#nueva-cita");
 
 const contenedorCitas = document.querySelector("#citas");
 
+// Clases
+
+class Citas {
+  constructor() {
+    this.arrCitas = [];
+  }
+}
+
+class UI {
+  imprimirAlerta(mensaje, tipoError) {
+    // Validar si el contenedor divMensaje existe para que no se creen multiples mensajes
+    if (document.querySelector(".divMensaje")) {
+      return;
+    }
+    // crear el mensaje
+    const divMensaje = document.createElement("div");
+    divMensaje.classList.add(
+      "divMensaje",
+      "text-center",
+      "alert",
+      "d-block",
+      "col-12",
+      "text-uppercase",
+      "font-weight-bold"
+    );
+
+    // agregar clase en base al tipo de error
+    if (tipoError === "error") {
+      divMensaje.classList.add("alert-danger");
+    } else {
+      divMensaje.classList.add("alert-success");
+    }
+
+    // texto del mensaje
+    divMensaje.textContent = mensaje;
+
+    // agregar al DOM
+    document
+      .querySelector("#contenido")
+      .insertBefore(divMensaje, document.querySelector(".agregar-cita"));
+
+    setTimeout(() => {
+      divMensaje.remove();
+    }, 3000);
+  }
+}
+
+const ui = new UI();
+const administrarCitas = new Citas();
+
 // Eventos
 eventos();
 
@@ -21,6 +71,8 @@ function eventos() {
   inputFecha.addEventListener("input", datosCita);
   inputHora.addEventListener("input", datosCita);
   inputSintomas.addEventListener("input", datosCita);
+
+  form.addEventListener("submit", nuevaCita);
 }
 
 // Objeto con la informacion de la cita
@@ -33,4 +85,20 @@ function datosCita(e) {
   citaObj[e.target.name] = e.target.value;
 
   console.log(citaObj);
+}
+
+// Valida y agrega una nueva cita a la clase de citas
+function nuevaCita(e) {
+  e.preventDefault();
+
+  // Extraer informacion del objeto de citas
+  const { mascota, propietario, telefono, fecha, hora, sintomas } = citaObj;
+
+  // validar
+  if (!mascota || !propietario || !telefono || !fecha || !hora || !sintomas) {
+    ui.imprimirAlerta("Todos los campos son obligatorios", "error");
+  } else if (isNaN(telefono)) {
+    ui.imprimirAlerta("Ingresa un número de telefono válido", "error");
+    return;
+  }
 }

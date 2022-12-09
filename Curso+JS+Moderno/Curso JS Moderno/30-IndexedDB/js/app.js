2cccaca2ca790dfdce5c6c50d3ac3a4433ed54a3
Dixon G <dxxn0101@gmail.com>
2022-12-09 17:03:35 -0600

    Introducción a IndexedDB

---
+++ Curso+JS+Moderno/Curso JS Moderno/30-IndexedDB/js/app.js
@@ -0,0 +1,11 @@
+// indexDB es un API de JS para almacenar grandes cantidades de datos estructurados
+
+// A diferencia  de LocalStorage puede almacenar string, booleans, incluso archivos, cualquier tipo de dato soportado por JS
+
+// no tiene "Limites" conocidos anque los archivos de más de 50mb va a pedir permiso
+
+// Soportada en todas las ultimas versiones de los navegadores
+
+// Es diferente a LocalStorage. LocalStorage es una buena solución para almacenar poca información ( como un carrito de compras abandonado o un JSON web token)
+
+// indexDB es una base de datos completa, pero hay que tener en cuenta que estos datos siguen siendo visibles para cualquiera por lo que no es recomendable almacenar passwords o tarjetas de crédito #FF0000

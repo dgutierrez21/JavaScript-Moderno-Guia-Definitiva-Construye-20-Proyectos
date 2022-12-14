41a2e6d8cf512b1b396be8895095fa287005b0ad
Dixon G <dxxn0101@gmail.com>
2022-12-14 15:24:59 -0600

    Como utilizar FETCH API

---
+++ Curso+JS+Moderno/Curso JS Moderno/35-FetchAPI/js/01-app.js
@@ -0,0 +1,23 @@
+const btnCargarTxt = document.querySelector("#cargarTxt");
+
+btnCargarTxt.addEventListener("click", obtenerDatos);
+
+function obtenerDatos() {
+  const url = "data/dato.txt";
+  fetch(url)
+    .then((respuesta) => {
+      console.log(respuesta);
+      console.log(respuesta.status);
+      console.log(respuesta.statusText);
+      console.log(respuesta.url);
+      console.log(respuesta.type);
+
+      return respuesta.text();
+    })
+    .then((datos) => {
+      console.log(datos);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}

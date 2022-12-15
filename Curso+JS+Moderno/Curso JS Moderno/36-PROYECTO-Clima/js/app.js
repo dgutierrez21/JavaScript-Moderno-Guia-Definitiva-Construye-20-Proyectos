b3b642d85c54fb9c872c59fd458152de321acf75
Dixon G <dxxn0101@gmail.com>
2022-12-15 08:53:56 -0600

    Primeros pasos con la App

---
+++ Curso+JS+Moderno/Curso JS Moderno/36-PROYECTO-Clima/js/app.js
@@ -0,0 +1,17 @@
+const contenedor = document.querySelector(".container"),
+  divResultado = document.querySelector("#resultado"),
+  form = document.querySelector("#formulario");
+
+window.addEventListener("load", () => {
+  form.addEventListener("submit", buscarClima);
+});
+
+function buscarClima(e) {
+  e.preventDefault();
+
+  // Validar
+
+  // consultar la api
+
+  console.log("buscando clima...");
+}

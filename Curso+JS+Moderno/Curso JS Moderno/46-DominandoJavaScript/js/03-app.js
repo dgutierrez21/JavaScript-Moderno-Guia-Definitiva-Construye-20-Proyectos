4082284e8bdb5cfca6a5019861685a1c0220325f
Dixon G <dxxn0101@gmail.com>
2022-12-21 12:59:05 -0600

    Coercion

---
+++ Curso+JS+Moderno/Curso JS Moderno/46-DominandoJavaScript/js/03-app.js
@@ -0,0 +1,20 @@
+// coerción
+
+// exiten dos tipos:
+
+// conversión automatica o implicita de valores de un tipo dado a otro
+
+const numero1 = "50";
+
+const numero2 = 30;
+
+// convierte numero dos a string
+console.log(numero1 + numero2);
+
+// conversión explicita
+
+console.log(Number(numero1) + numero2);
+
+const pedido = [1, 2, 3, 4];
+
+console.log(pedido.toString());

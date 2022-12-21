d8411b1ff2a60621a0a58c02c630f08ccb53a444
Dixon G <dxxn0101@gmail.com>
2022-12-21 09:06:58 -0600

    Funciones como Argumentos

---
+++ Curso+JS+Moderno/Curso JS Moderno/45-FunctionalJS/js/02-app.js
@@ -0,0 +1,9 @@
+const suma = (a, b) => a + b;
+
+const multiplicar = (a, b) => a * b;
+
+const sumarOmultiplicar = (fn) => fn(10, 20);
+
+console.log(sumarOmultiplicar(suma));
+
+console.log(sumarOmultiplicar(multiplicar));

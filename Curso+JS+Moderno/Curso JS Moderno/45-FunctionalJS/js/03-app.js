59faac2e64de919d900ca8dbb94c4c139a0deffe
Dixon G <dxxn0101@gmail.com>
2022-12-21 09:18:45 -0600

    Separar los Datos de las funciones

---
+++ Curso+JS+Moderno/Curso JS Moderno/45-FunctionalJS/js/03-app.js
@@ -0,0 +1,24 @@
+const carrito = [
+  { nombre: "Monitor 20 Pulgadas", precio: 500 },
+  { nombre: "Televisión 50 Pulgadas", precio: 700 },
+  { nombre: "Tablet", precio: 300 },
+  { nombre: "Audifonos", precio: 200 },
+  { nombre: "Teclado", precio: 50 },
+  { nombre: "Celular", precio: 500 },
+  { nombre: "Bocinas", precio: 300 },
+  { nombre: "Laptop", precio: 800 },
+];
+
+const resultado = carrito.filter((producto) => producto.precio > 400);
+
+console.log(resultado);
+
+console.log("****************");
+
+// separar los datos de las funciones
+
+const mayor400 = (producto) => producto.precio > 400;
+
+const resultado2 = carrito.filter(mayor400);
+
+console.log(resultado2);

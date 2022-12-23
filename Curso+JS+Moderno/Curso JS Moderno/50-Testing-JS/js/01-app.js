e9d1cbe8ce63a0a8c8d018f6d371fce4370ff9ab
Dixon G <dxxn0101@gmail.com>
2022-12-23 14:41:55 -0600

    Introducción al Testing

---
+++ Curso+JS+Moderno/Curso JS Moderno/50-Testing-JS/js/01-app.js
@@ -0,0 +1,26 @@
+// ventajas de hacer testing
+
+// -- mejorará la calidad de el software evitando bugs
+
+// --probar todos los diferentes escenarios puede ser complicado o tardado, pero hay herramientas que automatizan las pruebas de nuestros proyectos
+
+// -- liberar nuevas versiones sin la preocupación de que algo salga mal
+
+// ---- consideraciones con el testing ----
+
+// --- ¿Cuantas veces has agregado nuevas funciones a un proyecto existente pero desconoces si funciona bien con lo existente?
+
+//-- Tener pruebas hará que una persona que no ha mantenido un proyecto conozca que es lo que hace cada parte.
+
+// ---- Diferentes tipos de Testing ----
+// -- End to End - Más interactivo, simula algunos clicks, llenar formularios y asegurarse de que se muestre en pantalla lo que se desea.
+
+//-- Integración - Revisar que múltiples partes de nuestro proyecto funcionen bien.
+
+// -- Unit - Revisar que cada parte por si sola funcione bien. Static - Revisar por errores en el código mientras vas escribiendo.
+
+// ---- Herramientas para Testing ----
+//-- Cada tecnología tiene sus herramientas para Testing,
+//-- pero una muy popular es Jest, hay versiones para VueJS, Angular, TypeScript Node, React, etc. Es necesario tener instalado Node.js
+
+//-- Otra opción es Cypress que es una herramienta para hacer testings End to End.

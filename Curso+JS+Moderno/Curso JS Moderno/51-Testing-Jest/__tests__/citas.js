df25063ce9a2a7e8ef0464516a4af9cc74ec5152
Dixon G <dxxn0101@gmail.com>
2022-12-23 21:46:55 -0600

    Testing al Proyecto de Citas - Actualizar y Eliminar

--- Curso+JS+Moderno/Curso JS Moderno/51-Testing-Jest/__tests__/citas.js
+++ Curso+JS+Moderno/Curso JS Moderno/51-Testing-Jest/__tests__/citas.js
@@ -3,8 +3,11 @@ import Citas from "../js/classes/Citas.js";
 describe("Probar la clase de citas", () => {
   const citas = new Citas();
 
+  const id = Date.now();
+
   test("Agregar nueva cita", () => {
     const citaObj = {
+      id,
       mascota: "Firu",
       propietario: "Palmer Pamila",
       telefono: "002211",
@@ -13,12 +16,34 @@ describe("Probar la clase de citas", () => {
       sintomas: "No quiere comer",
     };
 
-    citaObj.id = Date.now();
-
     citas.agregarCita(citaObj);
 
     // Prueba
 
+    expect(citas).toMatchSnapshot();
+  });
+
+  test("Actualizar cita", () => {
+    const citaActualizada = {
+      id,
+      mascota: "Rocco",
+      propietario: "Palmer Pamila",
+      telefono: "002211",
+      fecha: "5-8-2022",
+      hora: "8:00",
+      sintomas: "No quiere comer",
+    };
+
+    citas.editarCita(citaActualizada);
+
+    // Prueba
+    expect(citas).toMatchSnapshot();
+  });
+
+  test("Eliminar cita", () => {
+    citas.eliminarCita(id);
+
+    // Prueba
     expect(citas).toMatchSnapshot();
   });
 });

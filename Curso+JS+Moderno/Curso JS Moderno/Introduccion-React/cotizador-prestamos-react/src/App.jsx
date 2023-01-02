9bb4f507e4ebe2eb66c1a0e26bcc4ea71c0627f4
Dixon G <dxxn0101@gmail.com>
2023-01-02 17:01:05 -0600

    Añadiendo un Input para las cantidades

--- Curso+JS+Moderno/Curso JS Moderno/Introduccion-React/cotizador-prestamos-react/src/App.jsx
+++ Curso+JS+Moderno/Curso JS Moderno/Introduccion-React/cotizador-prestamos-react/src/App.jsx
@@ -7,6 +7,12 @@ function App() {
 
       {/* Se recomienda usar esta sintaxis solo para componentes que tienen finalidad ser más reutilizables y pasarles contenido más dinamico */}
       {/* <Header></Header> */}
+
+      {/* Los inputs en react se cierran con /> al final */}
+      <input
+        type="range"
+        className="w-full h6 bg-gray-200 accent-lime-500 hover:accent-lime-600"
+      />
     </div>
   );
 }

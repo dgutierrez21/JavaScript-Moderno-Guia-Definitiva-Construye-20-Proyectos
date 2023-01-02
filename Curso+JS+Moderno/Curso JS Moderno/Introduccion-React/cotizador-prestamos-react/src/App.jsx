ef6a23e2bf8d7533a0f366fb84a39f5c681b961f
Dixon G <dxxn0101@gmail.com>
2023-01-02 16:36:45 -0600

    ¿Qué es JSX?

--- Curso+JS+Moderno/Curso JS Moderno/Introduccion-React/cotizador-prestamos-react/src/App.jsx
+++ Curso+JS+Moderno/Curso JS Moderno/Introduccion-React/cotizador-prestamos-react/src/App.jsx
@@ -1,11 +1,24 @@
-
-
 function App() {
+  let mensaje;
 
+  const verOfal = true;
+
+  if (verOfal) {
+    mensaje = "Hola mundo en React JS";
+  } else {
+    mensaje = "Ha ocurrido un error";
+  }
 
   return (
-    <h1 className="text-6xl text-center font-bold">Hola Mundo en React</h1>
-  )
+    <div className="my-20 max-w-lg mx-auto bg-white text-black shadow p-10">
+      <p>
+        {verOfal
+          ? "Se inicio sesión correctamente"
+          : "No se pudo iniciar sesión"}
+      </p>
+      <p>{mensaje} </p>
+    </div>
+  );
 }
 
-export default App
+export default App;
